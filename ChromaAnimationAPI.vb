b06b308387a947559ef944f44b39b9fb1cdaeb4e
Timothy Graupmann <tgraupmann@gmail.com>
2022-03-31 21:22:27 -0700

    Setup a ChromaAnimationAPI module

---
+++ ChromaAnimationAPI.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Runtime.InteropServices
+
+Module ChromaAnimationAPI
+
+#If X64 Then
+    Const DLL_NAME As String = "CChromaEditorLibrary64"
+#Else
+    Const DLL_NAME As String = "CChromaEditorLibrary"
+#End If
+
+    REM /// <summary>
+    REM /// Return the sum of colors
+    REM /// EXPORT_API int PluginAddColor(const int color1, const int color2);
+    REM /// </summary>
+    <DllImport(DLL_NAME, CallingConvention:=CallingConvention.Cdecl)>
+    Private Function PluginAddColor(color1 As Integer, color2 As Integer) As Integer
+    End Function
+
+    REM /// <summary>
+    REM /// Return the sum of colors
+    REM /// </summary>
+    Public Function AddColor(color1 As Integer, color2 As Integer) As Integer
+        Dim result As Integer = PluginAddColor(color1, color2)
+        Return result
+    End Function
+
+End Module
--- Module1.vb
+++ Module1.vb
@@ -2,6 +2,10 @@
 
     Sub Main()
 
+        Dim color1 As Integer = &HEFF0000
+        Dim color2 As Integer = &HE0000FF
+        Dim result As Integer = ChromaAnimationAPI.AddColor(color1, color2)
+
     End Sub
 
 End Module
